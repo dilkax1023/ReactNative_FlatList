7389bda04cf791ddb5644d09795100f6eb06ff8a
Hakim ghani <57526200+dilkax1023@users.noreply.github.com>
2020-08-15 14:20:09 +0200

    exercise 2 done

--- App.js
+++ App.js
@@ -1,21 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+	SafeAreaView,
+	View,
+	FlatList,
+	StyleSheet,
+	Text,
+	StatusBar,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import SvgUri from 'react-native-svg';
+
+const App = () => {
+	const [countries, setCountries] = useState([]);
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const fetchData = async () => {
+		const url = 'http://restcountries.eu/rest/v2/all';
+		const response = await fetch(url);
+		const data = await response.json();
+		console.log(data);
+		setCountries(data);
+	};
+
+	// const renderItem = ({ country }) => (
+	// 	<Text style={styles.title}>
+	// 		{country.name} - {country.capital}
+	// 	</Text>
+	// );
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text style={styles.h1}>Country - Capital</Text>
+			<FlatList
+				data={countries}
+				renderItem={({ item }) => {
+					return (
+						<View>
+							<Text style={styles.item}>
+								{item.name} - {item.capital}
+							</Text>
+						</View>
+					);
+				}}
+				keyExtractor={(item) => item.name}
+			/>
+		</SafeAreaView>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		marginTop: StatusBar.currentHeight || 0,
+	},
+	h1: {
+		textAlign: 'center',
+		fontSize: 30,
+		marginVertical: 50,
+		color: '#4589ff',
+		fontWeight: 'bold',
+	},
+	item: {
+		backgroundColor: '#119191',
+		padding: 20,
+		marginVertical: 8,
+		marginHorizontal: 16,
+		color: '#fff',
+		fontSize: 16,
+		textAlign: 'center',
+		letterSpacing: 1.3,
+		fontWeight: 'bold',
+	},
+	title: {
+		fontSize: 32,
+	},
+	stretch: {
+		width: 50,
+		height: 200,
+		resizeMode: 'stretch',
+	},
 });
+
+export default App;
